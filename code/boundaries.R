9dedb2bf472d6db5507932ef37798b04b410d53d
Robin Lovelace <rob00x@gmail.com>
2020-05-14 17:44:17 +0100

    Update boundaries

--- code/boundaries.R
+++ code/boundaries.R
@@ -3,6 +3,7 @@ library(tidyverse)
 counties = ukboundaries::duraz("https://borders.ukdataservice.ac.uk/ukborders/easy_download/prebuilt/kml/England_ct_2001_clipped.zip")
 counties = sf::read_sf("england_ct_2001_clipped.kml")
 mapview::mapview(counties)
+# Counties and Unitary Authorities (December 2017) Boundaries UK
 uas_uk = sf::read_sf("https://opendata.arcgis.com/datasets/6638c31a8e9842f98a037748f72258ed_3.kml?outSR=%7B%22latestWkid%22%3A27700%2C%22wkid%22%3A27700%7D")
 uas_uk = uas_uk %>% select(matches("cty"))
 plot(uas_uk)
