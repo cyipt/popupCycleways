3b655d525305e8f1fccc8697e0970d275e75f666
Robin Lovelace <rob00x@gmail.com>
2020-05-14 12:15:56 +0100

    Update cycleway overlay script

--- code/explore-existing-popup-integration-birmingham.R
+++ code/explore-existing-popup-integration-birmingham.R
@@ -20,4 +20,28 @@ uas_en = readRDS("uas_en.Rds")
 region = uas_en %>% filter(ctyua17nm == region_name)
 mapview::mapview(region)
 cycleways_region = cycleways_en[region, ]
+# cycleways_region_long = cycleways_region %>% 
+#   mutate(length = round(as.numeric(st_length(.)))) %>% 
+#   filter(length > 50)
+# summary(cycleways_region_long$length)
 source("code/build.R")
+m = tm_shape(r_pct_top) + tm_lines(col = "blue", lwd = 6, alpha = 0.6) +
+  tm_shape(r_pct_top_n) + tm_text("name") +
+  tm_shape(cycleways_region) + tm_lines() +
+  tm_basemap(server = s) +
+  tm_scale_bar()
+m
+cycleway_buffer = stplanr::geo_buffer(cycleways_region, dist = 50) %>% sf::st_union()
+r_pct_top_no_cycleways = sf::st_difference(r_pct_top, cycleway_buffer)
+# r_pct_top_no_cycleways = r_pct_top[cycleway_buffer, , op = sf::st_disjoint]
+r_pct_top_no_cycleways = r_pct_top_no_cycleways %>% 
+  st_cast("LINESTRING") %>% 
+  mutate(group_length = round(as.numeric(st_length(.)))) %>% 
+  filter(group_length > 50)
+
+m = tm_shape(r_pct_top_no_cycleways) + tm_lines(col = "blue", lwd = 6, alpha = 0.6) +
+  tm_shape(r_pct_top_n) + tm_text("name") +
+  tm_shape(cycleways_region_long) + tm_lines() +
+  tm_basemap(server = s) +
+  tm_scale_bar()
+m
